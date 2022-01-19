be2a83b631a9ac517045ff5b62006a2b96b95c97
urigroisman <67598809+urigroisman@users.noreply.github.com>
2022-01-18 23:23:40 -0300

    vectores

--- functions/Modulo_Vector.cpp
+++ functions/Modulo_Vector.cpp
@@ -4,28 +4,31 @@
 
 //using namespace std;
 
-double Modulo_Vector(double x1, double x2, double x3);
+double Modulo_Vector(double a, double b, double c);
 double Vector [3][1];
 
 int main ()
 {
 	double x1,x2,x3,Mod;
 	
-
 	std::cout << "input x1 " << std::endl;
-	std::cin >> x1;
+	std::cin >> Vector[0][0];
 
 	std::cout << "input x2 " << std::endl;
-	std::cin >> x2;
+	std::cin >> Vector[1][0];
 
 	std::cout << "input x3 "<< std::endl;
-	std::cin >> x3;
+	std::cin >> Vector[2][0];
 
-	vector[3],[1] = {{x1},{x2},{x3}};
+//	Vector[3][1] = {{x1},{x2},{x3}};
 
-	// calc Modulo del Vector
+// calc Modulo del Vector
 	std::cout << "El Modulo del vector es " << std::endl;
-	Mod = Modulo_Vector(x1,x2,x3);
+// sin usar indices
+// Mod = Modulo_Vector(x1,x2,x3);
+
+// con indices	
+	Mod = Modulo_Vector(Vector[0][0],Vector[1][0],Vector[2][0]);
 
 	std::cout << Mod << std::endl;
 
@@ -33,9 +36,9 @@ int main ()
 	return 0;
 }
 
-double Modulo_Vector(double x1,double x2,double x3)
+double Modulo_Vector(double a,double b,double c)
 {
 	double Mod;
-	Mod = sqrt(pow(x1,2)+pow(x2,2)+pow(x3,2));
+	Mod = sqrt(pow(a,2)+pow(b,2)+pow(c,2));
 	return Mod;
 }
